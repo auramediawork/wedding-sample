8ce8be81bb19b9b13199364566712e873fc8128d
auramediawork <262653737+auramediawork@users.noreply.github.com>
2026-02-20 02:53:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
-const envelope=document.querySelector(".envelope-container");
 
-envelope.addEventListener("click",()=>{
+/* OPEN ENVELOPE + MUSIC */
+const env=document.getElementById("envelope");
+
+env.addEventListener("click",()=>{
 document.querySelector(".envelope").classList.add("open");
 
 setTimeout(()=>{
-envelope.style.display="none";
+env.style.display="none";
 document.getElementById("music").play();
-},1200);
+},1000);
 });
 
-/* SCROLL REVEAL */
+/* SCROLL REVEAL ANIMATION */
 const sections=document.querySelectorAll(".section");
 
 window.addEventListener("scroll",()=>{
 sections.forEach(sec=>{
-if(sec.getBoundingClientRect().top<window.innerHeight-100){
-sec.style.opacity="1";
-sec.style.transform="translateY(0)";
+if(sec.getBoundingClientRect().top < window.innerHeight-100){
+sec.classList.add("show");
 }
 });
 });
+
+/* GOLD PARTICLES BACKGROUND */
+for(let i=0;i<25;i++){
+let p=document.createElement("div");
+p.className="particle";
+p.style.left=Math.random()*100+"%";
+p.style.animationDuration=(5+Math.random()*5)+"s";
+document.body.appendChild(p);
+}
